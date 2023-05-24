a8f13df01442ca5454dad03b41363f1e759bd81e
Akash Mondal <a98mondal@gmail.com>
2023-05-24 22:54:25 +0530

    add functionality to generate from all files inside a directory
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- cv-gen.py
+++ cv-gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from typing import Any
-import sys
+from os import path, listdir
 from services import DataScv
 from txparser import Parser
 from log import debug_on, debug_print
@@ -12,31 +12,41 @@ import argparse
 parser = argparse.ArgumentParser(description="Generate CV from tex file")
 
 parser.add_argument("-p", "--profile", help="path to profile data file", default="profile-data.yaml")
-parser.add_argument("-i", "--input", help="path to input file", default="input.tex")
-parser.add_argument("-o", "--output", help="path to output file", default="output.tex")
+parser.add_argument("-i", "--input", help="path to input file or directory", default="input")
+parser.add_argument("-o", "--output", help="path to output file or directory", default="output")
 args = parser.parse_args()
 
 profile_data_path = args.profile
-input_file_path = args.input
-output_file_path = args.output
+input_path = args.input
+output_path = args.output
+io_path_map: dict[str, str] = {}
+
+if(path.isfile(input_path)):
+  io_path_map[input_path] = output_path
+else:
+  for file in listdir(input_path):
+    io_path_map[input_path + '/' + file] = output_path + '/' + file
+
 
 yaml = DataScv(profile_data_path)
 
 yaml.load_yaml()
 
-#load tex file
-with open(input_file_path, "r") as tex_file:
-  tex = tex_file.read()
-  debug_print("tex file loaded")
+for input_file_path, output_file_path in io_path_map.items():
+  #load tex file
+  with open(input_file_path, "r") as tex_file:
+    tex = tex_file.read()
+    debug_print("tex file loaded")
 
-  #parse tex file
-  parser = Parser()
-  parser.generate(tex)
-  output = parser.exec(yaml.get_data())
+    #parse tex file
+    parser = Parser()
+    parser.generate(tex)
+    output = parser.exec(yaml.get_data())
 
-  #write to output file
-  with open(output_file_path, "w") as output_file:
-    output_file.write(str(output))
+    #write to output file
+    with open(output_file_path, "w") as output_file:
+      output_file.write(str(output))
 
-    debug_print("tex file written")
-    print("Done!")
+      debug_print(output_file_path + ": file written!")
+      
+print("Done!")
